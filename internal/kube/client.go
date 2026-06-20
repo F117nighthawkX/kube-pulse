e2162c717a59bc0090e2224dd1a392ecf10a7323
F117nighthawkX <hayes.k50@gmail.com>
2026-06-20 16:16:10 -0400

    feat: Create Kubernetes client

--- internal/kube/client.go
+++ internal/kube/client.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-func CreateNewClient() {
+func CreateNewClient() (kubernetes.Interface, error) {
 	fmt.Println("Creating new Kubernetes client")
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		fmt.Printf("error getting user's home directory: %v", err)
+		return nil, fmt.Errorf("error getting user's home directory: %v", err)
 	}
 
 	kubeconfigPath := filepath.Join(homeDir, ".kube", "config")
 	fmt.Printf("Kubernetes config path: %s\n", kubeconfigPath)
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		return nil, fmt.Errorf("error building Kubernetes config: %v", err)
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("error creating Kubernetes client: %v", err)
+	}
+
+	fmt.Printf("Kubernetes client created successfully: %T\n", client)
+	return client, nil
 }
