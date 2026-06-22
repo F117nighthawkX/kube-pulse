0c3373893cfe2596586b16dd9071b93f7ddfffea
F117nighthawkX <hayes.k50@gmail.com>
2026-06-21 22:11:59 -0400

    feat: Check for Container issues

--- internal/kube/health/pod_health.go
+++ internal/kube/health/pod_health.go
@@ -22,15 +22,21 @@ func AnalyzePods(pods []corev1.Pod) []PodHealth {
 		health := PodHealth{
 			Namespace: pod.Namespace,
 			Name:      pod.Name,
-			// The default auto-fill from VS Code used `len(pod.Spec.Containers)` here, but that seems to
-			// be the desired state defined in the pod spec yaml. Need to use `len(pod.Status.ContainerStatuses)`
+			// The auto-fill from VS Code used `len(pod.Spec.Containers)` here, but that seems to be the
+			// desired state defined in the pod spec yaml. Need to use `len(pod.Status.ContainerStatuses)`
 			// which gives the runtime status reported by the cluster.
 			Ready:    fmt.Sprintf("%d/%d", readyContainers(pod), len(pod.Status.ContainerStatuses)),
 			Status:   string(pod.Status.Phase),
 			Restarts: countRestarts(pod),
 			Node:     pod.Spec.NodeName,
-			Issues:   phaseIssues(pod),
+			Issues:   []string{},
 		}
+
+		// VS Code auto-fill suggested the `...` elipsis syntax here, which seems to share some similarity
+		// to JavaScript's spread operator. Go calls it a variadic function, and it has some notable
+		// differences. See https://go101.org/article/function.html
+		health.Issues = append(health.Issues, phaseIssues(pod)...)
+		health.Issues = append(health.Issues, containerIssues(pod)...)
 
 		healthStatuses = append(healthStatuses, health)
 		//fmt.Printf("Analyzed pod: %s/%s - Status: %s\n", health.Namespace, health.Name, health.Status)
@@ -70,6 +76,35 @@ func phaseIssues(pod corev1.Pod) []string {
 	// Looks like PodUnknown has been deprecated, will never be set
 
 	issues = append(issues, fmt.Sprintf("Pod is in %s phase", phase))
+
+	return issues
+}
+
+func containerIssues(pod corev1.Pod) []string {
+	var issues []string
+	for _, containerStatus := range pod.Status.ContainerStatuses {
+		containerName := containerStatus.Name
+
+		if !containerStatus.Ready {
+			msg := fmt.Sprintf("Container %s is not ready", containerName)
+			issues = append(issues, msg)
+		}
+
+		if containerStatus.RestartCount > 0 {
+			msg := fmt.Sprintf("Container %s has restarted %d times", containerName, containerStatus.RestartCount)
+			issues = append(issues, msg)
+		}
+
+		if containerStatus.State.Waiting != nil {
+			msg := fmt.Sprintf("Container %s is waiting: %s", containerName, containerStatus.State.Waiting.Reason)
+			issues = append(issues, msg)
+		}
+
+		if containerStatus.State.Terminated != nil {
+			msg := fmt.Sprintf("Container %s is terminated: %s", containerName, containerStatus.State.Terminated.Reason)
+			issues = append(issues, msg)
+		}
+	}
 
 	return issues
 }
